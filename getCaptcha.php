2ae4a55b33f65862e471d3c0e0cb93deb8b9cfb0
carry0987 <carry0987@users.noreply.github.com>
2019-04-28 17:39:25 +0800

    Upload

---
+++ getCaptcha.php
@@ -0,0 +1,31 @@
+<?php
+include 'class/SVGCaptcha.php';
+
+if (isset($_POST['svgcaptcha_difficulty']) && !empty($_POST['svgcaptcha_difficulty'])) {
+    $obj = null; 
+    switch ($_POST['svgcaptcha_difficulty']) {
+        case 'easy':
+            $obj = SVGCaptcha::getInstance(4, $width = 300, $height = 130, $difficulty = SVGCaptcha::EASY);
+            $c = $obj->getSVGCaptcha();
+            echo $c[1];
+            //Answer
+            //echo $c[0];
+            break;
+        case 'medium':
+            $obj = SVGCaptcha::getInstance(4, $width = 300, $height = 130, $difficulty = SVGCaptcha::MEDIUM);
+            $c = $obj->getSVGCaptcha();
+            echo $c[1];
+            //Answer
+            //echo $c[0];
+            break;
+        case 'hard':
+            $obj = SVGCaptcha::getInstance(4, $width = 300, $height = 130, $difficulty = SVGCaptcha::HARD);
+            $c = $obj->getSVGCaptcha();
+            echo $c[1];
+            //Answer
+            //echo $c[0];
+            break;
+        default:
+            break;
+    }
+}
